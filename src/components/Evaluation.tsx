812dca37362722e3ec15a57f02366e883c6f76e5
Steve Kienbaum <steve.kienbaum@web.de>
2023-02-09 13:43:14 +0100

    Bootstrap

--- src/components/Evaluation.tsx
+++ src/components/Evaluation.tsx
@@ -1,12 +1,27 @@
 import { FunctionComponent } from "react";
+import { Route, Routes } from "react-router";
+import { LinkContainer } from "react-router-bootstrap";
+import Form from "./Form";
 
  
 const Evaluation = () => {
-  return (<div>
+  return (
+    <>
+    <div>
     <h1>Access Evaluation</h1>
     <label htmlFor="">Enter Code: </label>
     <input className="code" type="text" />
-  </div>);
+    <LinkContainer to="/Form">
+      <button className='btn btn-primary'>Start</button>
+    </LinkContainer>
+  </div>
+  <Routes>
+    {/* Define route of the different pages */}
+    <Route path="/Form" element={<Form />} />
+  </Routes>
+    </>
+  
+);
 }
  
 export default Evaluation;
